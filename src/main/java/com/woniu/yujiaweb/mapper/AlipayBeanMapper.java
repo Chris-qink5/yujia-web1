e76533c756ec479054273c834e720ab2c9b5a20c
huangdong121 <851335780@qq.com>
2021-03-12 15:27:09 +0800

    修改shiro

---
+++ src/main/java/com/woniu/yujiaweb/mapper/AlipayBeanMapper.java
@@ -0,0 +1,17 @@
+package com.woniu.yujiaweb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.woniu.yujiaweb.domain.AlipayBean;
+import com.woniu.yujiaweb.domain.Role;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author qk
+ * @since 2021-03-06
+ */
+public interface AlipayBeanMapper extends BaseMapper<AlipayBean> {
+
+}
