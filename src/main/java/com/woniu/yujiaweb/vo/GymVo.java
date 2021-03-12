e264d6ff53210a394d88381ccafc456e11ded576
huangdong121 <851335780@qq.com>
2021-03-12 15:26:53 +0800

    修改shiro

---
+++ src/main/java/com/woniu/yujiaweb/vo/GymVo.java
@@ -0,0 +1,13 @@
+package com.woniu.yujiaweb.vo;
+
+import lombok.Data;
+
+@Data
+public class GymVo {
+    private String gymTel;
+    private String gymAddress;
+    private String gymDescription;
+    private String gymPhoto;
+    private String gymName;
+
+}
---
+++ src/main/java/com/woniu/yujiaweb/vo/PageVo.java
@@ -0,0 +1,12 @@
+package com.woniu.yujiaweb.vo;
+
+
+import lombok.Data;
+
+
+
+@Data
+public class PageVo {
+    private Integer current;
+    private Integer size;
+}
